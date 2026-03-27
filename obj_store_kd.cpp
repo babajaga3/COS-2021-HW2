7e0ad045ac6cacda20e222bd2ad57f35825ed677
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2026-03-27 18:41:32 +0200

    :zap: Add helper insert node method

--- obj_store_kd.cpp
+++ obj_store_kd.cpp
@@ -1,19 +1,50 @@
 #include "obj_store_kd.hpp"
 
-// Constructors
-ObjStoreKD::ObjStoreKD() {
-    this->root = nullptr;
+ObjStoreKD::ObjStoreKD() : root(nullptr) {}
+
+ObjStoreKD::ObjStoreKD(Node<Object> *new_root) : root(new_root) {}
+
+void ObjStoreKD::insert(Node<Object> *new_node, const int dim, Node<Object> *start) {
+    Node<Object> *current = start;
+
+    // if root is empty, add node as root
+    if (this->root == nullptr) {
+        this->root = new_node;
+    }
+
+    // if current is empty, use root as current
+    if (current == nullptr) {
+        current = this->root;
+    }
+
+    // if smaller, go to the left
+    if (at(current, dim) < at(current, dim)) {
+        // if left is empty, add node as the left node
+        if (current->get_left() == nullptr) {
+            current->set_left(new_node);
+            return;
+        }
+
+        // if not, go to left node, start over
+        insert(new_node, dim + 1, current->get_left());
+    } else if (at(current, dim) >= at(current, dim)) {
+        // if right is empty, add node as the right node
+        if (current->get_right() == nullptr) {
+            current->set_right(new_node);
+            return;
+        }
+
+        // if not, go to right node, start over
+        insert(new_node, dim + 1, current->get_right());
+    }
 }
 
-ObjStoreKD::ObjStoreKD(Node<Object> *new_root) {
-    this->root = new_root;
-}
+float ObjStoreKD::at(const Node<Object> *node, const int &index) {
+    const Point2D point_2d = node->get_data()->GetLoc();
 
-// required functions
-bool ObjStoreKD::insert(Object obj) {
+    if (index % 2 == 0) {
+        return point_2d.x;
+    }
 
-}
-
-void ObjStoreKD::find_all_within_radius(Point2D point_2d, float radius, std::vector<Object *> &objs) const {
-
+    return point_2d.y;
 }
--- obj_store_kd.hpp
+++ obj_store_kd.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include <vector>
 
 #include "node.hpp"
 #include "object.hpp"
@@ -12,6 +11,6 @@ public:
     ObjStoreKD();
     ObjStoreKD(Node<Object> *new_root);
 
-    bool insert(Object obj);
-    void find_all_within_radius(Point2D point_2d, float radius, std::vector<Object*> &objs) const;
+    void insert(Node<Object> *new_node, int dim, Node<Object> *start);
+    static float at(const Node<Object> *node, const int &index);
 };
