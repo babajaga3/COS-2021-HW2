b46f88fa0dc5cf7f6cd9a534158afd06ef387661
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2026-03-28 13:25:49 +0200

    🚧 Testing out the program

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+#include "object.hpp"
+#include "obj_store_kd.hpp"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    const Object obj1 = Object(1, {1, 2}, {3, 4});
+
+    auto *obj_store = new ObjStoreKD();
+
+    obj_store->insert_main(obj1);
+
+    obj_store->print_main();
 }
+
+/*
+ * References used:
+ * - https://www.geeksforgeeks.org/cpp/unordered_set-in-cpp-stl/
+ * - https://www.geeksforgeeks.org/cpp/operator-overloading-cpp/
+ */
