79bfe38b4057135b31a84a30e3c25097f2118422
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2026-03-28 13:25:27 +0200

    :zap: add set for keeping track of ids

--- obj_store_kd.cpp
+++ obj_store_kd.cpp
@@ -1,8 +1,10 @@
 #include "obj_store_kd.hpp"
 
-ObjStoreKD::ObjStoreKD() : root(nullptr) {}
+#include <iostream>
 
-ObjStoreKD::ObjStoreKD(Node<Object> *new_root) : root(new_root) {}
+ObjStoreKD::ObjStoreKD() : root(nullptr), existing_ids() {}
+
+ObjStoreKD::ObjStoreKD(Node<Object> *new_root) : root(new_root), existing_ids() {}
 
 void ObjStoreKD::insert(Node<Object> *new_node, const int dim, Node<Object> *start) {
     Node<Object> *current = start;
@@ -10,6 +12,7 @@ void ObjStoreKD::insert(Node<Object> *new_node, const int dim, Node<Object> *sta
     // if root is empty, add node as root
     if (this->root == nullptr) {
         this->root = new_node;
+        return;
     }
 
     // if current is empty, use root as current
@@ -17,8 +20,12 @@ void ObjStoreKD::insert(Node<Object> *new_node, const int dim, Node<Object> *sta
         current = this->root;
     }
 
+    if (new_node == nullptr) {
+        return;
+    }
+
     // if smaller, go to the left
-    if (at(current, dim) < at(current, dim)) {
+    if (at(new_node, dim) < at(current, dim)) {
         // if left is empty, add node as the left node
         if (current->get_left() == nullptr) {
             current->set_left(new_node);
@@ -27,7 +34,7 @@ void ObjStoreKD::insert(Node<Object> *new_node, const int dim, Node<Object> *sta
 
         // if not, go to left node, start over
         insert(new_node, dim + 1, current->get_left());
-    } else if (at(current, dim) >= at(current, dim)) {
+    } else if (at(new_node, dim) >= at(current, dim)) {
         // if right is empty, add node as the right node
         if (current->get_right() == nullptr) {
             current->set_right(new_node);
@@ -39,8 +46,47 @@ void ObjStoreKD::insert(Node<Object> *new_node, const int dim, Node<Object> *sta
     }
 }
 
-float ObjStoreKD::at(const Node<Object> *node, const int &index) {
-    const Point2D point_2d = node->get_data()->GetLoc();
+bool ObjStoreKD::insert_main(Object obj) {
+
+    // verify that the id doesn't already exist
+    if (existing_ids->find(obj.GetID()) != existing_ids->end()) {
+        return false;
+    }
+
+    // add id to set
+    existing_ids->insert(obj.GetID());
+
+    Node<Object> *new_node = new Node<Object>(obj);
+
+    this->insert(new_node, 0, nullptr);
+
+    return true;
+}
+
+void ObjStoreKD::print_main() const {
+    print(root, 0);
+}
+
+void ObjStoreKD::print(Node<Object>* node, int dim) {
+    // Base case: If node is null, return
+    if (node == nullptr) return;
+
+    // Print current node with indentation based on dim
+    for (int i = 0; i < dim; i++) std::cout << "  ";
+    std::cout << "(";
+    for (size_t i = 0; i < 2; i++) {
+        std::cout << at(node, i);
+        if (i < 2 - 1) std::cout << ", ";
+    }
+    std::cout << ")" << std::endl;
+
+    // Recursively print left and right children
+    print(node->get_left(), dim + 1);
+    print(node->get_right(), dim + 1);
+}
+
+float ObjStoreKD::at(Node<Object> *node, const int &index) {
+    const Point2D point_2d = node->get_data().GetLoc();
 
     if (index % 2 == 0) {
         return point_2d.x;
--- obj_store_kd.hpp
+++ obj_store_kd.hpp
@@ -1,16 +1,22 @@
 #pragma once
 
+#include <unordered_set>
+
 #include "node.hpp"
 #include "object.hpp"
 
 class ObjStoreKD {
 private:
     Node<Object> *root;
+    std::unordered_set<int> *existing_ids;
 
 public:
     ObjStoreKD();
     ObjStoreKD(Node<Object> *new_root);
 
     void insert(Node<Object> *new_node, int dim, Node<Object> *start);
-    static float at(const Node<Object> *node, const int &index);
+    bool insert_main(Object obj);
+    static void print(Node<Object> *node, int dim);
+    static float at(Node<Object> *node, const int &index);
+    void print_main() const;
 };
